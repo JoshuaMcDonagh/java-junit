aa2cc27ef18f1a8de9854628ea9add2124e935e7
JoshuaMcDonagh <joshuamichaelmcdonagh@gmail.com>
2024-10-28 11:26:03 +0000

    feat: add Compass class with intital fields and rotate method signiture.

---
+++ src/main/java/Compass.java
@@ -0,0 +1,26 @@
+public class Compass {
+
+    // Fields
+
+    private enum Point {
+        NORHT,
+        EAST,
+        SOUT,
+        WEST;
+    }
+
+    private enum Direction {
+        LEFT,
+        RIGHT;
+    }
+
+    private Point point; // Current point direction
+
+    // Methods
+
+    public Point rotate (Point currentDirection, Direction directionToTurn) {
+        return null;
+    }
+
+}
+
